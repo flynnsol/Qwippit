32316f0a1b21f802c694d70c9d65a331389b0231
Flynn <74693969+flynnsol@users.noreply.github.com>
2023-05-08 21:55:56 -0500

    admin authentication and only redirects working for dashboard home

--- qwippit/dashboard/routes.py
+++ qwippit/dashboard/routes.py
@@ -1,8 +1,48 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash, jsonify
+from flask_login import current_user, login_user, logout_user
+from sqlalchemy import func
+
+from qwippit import bcrypt
+from qwippit.models import User
+from qwippit.users.forms import LoginForm
 
 dash = Blueprint('dash', __name__)
 
 
-@dash.route("/", subdomain='dash')
+@dash.route("/home", subdomain='dash')
 def dash_home():
-    return render_template('dash/home.html', title='Dashboard Home')
+    if request.referrer:
+        if current_user.is_authenticated:
+            if len(current_user.roles) > 0:
+                if current_user.roles[0].name == 'Admin':
+                    return render_template('dash/home.html', title='Dashboard Home')
+            else:
+                flash('You do not have access to that page.', 'danger')
+                return redirect(url_for('dash.dash_redirect'))
+        else:
+            return redirect(url_for('dash.dash_signin'))
+    else:
+        return redirect(url_for('dash.dash_redirect'))
+
+
+@dash.route("/", subdomain='dash')
+def dash_redirect():
+    logout_user()
+    return redirect(url_for('dash.dash_signin'))
+
+
+@dash.route("/signin", subdomain='dash', methods=['GET', 'POST'])
+def dash_signin():
+    if current_user.is_authenticated:
+        return redirect(url_for('dash.dash_home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter(func.lower(User.email) == func.lower(form.email.data)).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dash.dash_home'))
+        else:
+            flash('Sign In Unsuccessful. Please check email and password.', 'danger')
+            return render_template('dash/signin.html', title='Dashboard Sign In', form=form)
+    return render_template('dash/signin.html', title="Dashboard Sign In", form=form)
